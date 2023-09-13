e7ea17be39d9d566d45d7803dfdbaa1b93d2442d
Simon Hefvelin Stridsberg <simon_nr6@hotmail.com>
2023-09-13 15:02:02 +0200

    can delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,12 @@ export default function App() {
     })
   }
 
+  function deleteTodo (id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -55,7 +61,7 @@ export default function App() {
             onChange={e => toggleTodo(todo.id, e.target.checked)}/>
             {todo.title}
           </label>
-          <button className="btn btn-danger">Delete</button>
+          <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
          </li>
           )
         })}
